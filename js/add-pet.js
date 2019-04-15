ebc52153f22a315ad5bd3daa2b5bbcf117401b1f
Charles Clemens <cclemensjr@gmail.com>
2019-04-15 15:53:14 -0700

    Fixed local storage bug

--- js/add-pet.js
+++ js/add-pet.js
@@ -14,10 +14,10 @@ function submitForm(event){
   var newType = event.target.petType.value;
   var newAge = event.target.petAge.value;
   var newColor = event.target.petColor.value;
-  console.log('event listener for submit' + newName + ' ' +newType);
 
-  var addNewAnimal = new Animal(newName,newType, newAge, newColor);
-  addNewAnimal.saveToLocalStorage();
+  new Animal(newName,newType, newAge, newColor);
+
+  localStorage.setItem('animalObjectArray', JSON.stringify(animalObjectArray));
 }
 
 submit.addEventListener('submit', submitForm);
