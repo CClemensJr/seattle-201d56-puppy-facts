2ef2d58357be2f7d6385fe7daa309117eeab9d00
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-04-15 15:10:20 -0700

    eventlistener for submit works

--- js/add-pet.js
+++ js/add-pet.js
@@ -6,6 +6,8 @@ var animalTypeArray = ['dog', 'cat', 'pig', 'bird', 'rabbit'];
 var submit = document.getElementById('userInputForm');
 
 function submitForm(event){
+  event.preventDefault();
+  console.log(event);
   var newName = event.target.petName.value;
   var newType = event.target.petType.value;
   var newAge = event.target.petAge.value;
