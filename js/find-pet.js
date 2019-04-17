3f2d869c9498e803a503a9442138bf806e51be43
jshin83 <jen.kyongah.lee@gmail.com>
2019-04-17 14:46:16 -0700

    select dropdown functionality complete - only generates by type

--- js/find-pet.js
+++ js/find-pet.js
@@ -3,32 +3,61 @@
 var animalObjectArray = [];
 var ulEl;
 var liEl;
-var typeFilter;
+var typeFilter = '';
+var i;
+var divEl;
+
+/*
+Stretch Goal:
+animalObjectArray.includes("type")
+false
+animalObjectArray[0].type.includes("cat")
+true
+animalObjectArray[0].type.includes("dog")
+false
+animalObjectArray[0].type.includes("pig")
+false
+animalObjectArray[1].type.includes("pig")
+true
+
+console tests - dynamically create select on html page to only show available types
+*/
 
 function render(){
-  var divEl = document.getElementById('elements');
-
-  for (var i = 0; i < animalObjectArray.length; i++){
-    console.log(animalObjectArray[i]);
-
-    ulEl = document.createElement('ul');
-    ulEl.id = animalObjectArray[i].type;
-    liEl = document.createElement('li');
-    var imgEl = document.createElement('img');
-    imgEl.src = '../assets/imganimals/' + animalObjectArray[i].type + '.jpg';
-    liEl.appendChild(imgEl);
-    ulEl.appendChild(liEl);
-    //create li elements with each object property
-    createLi(i, 'Name:', 'name');
-    createLi(i, 'Animal Type:', 'type');
-    createLi(i, 'Age:', 'age');
-    createLi(i, 'Color:', 'color');
-
-    divEl.appendChild(ulEl);
+  divEl = document.getElementById('elements');
+  //divEl.innerHTML = '';
+  for (i = 0; i < animalObjectArray.length; i++){
+    if(typeFilter) {
+      if(animalObjectArray[i].type === typeFilter) {
+        console.log('about to attach animal ', i);
+        showAvailableAnimals();
+      }
+    } else {
+      console.log('if typefilter is on, should not be in this loop!!!');
+      showAvailableAnimals();
+    }
   }
+  typeFilter = '';
 }
 
-//function to show only images by type
+//helper function to show all available animals
+function showAvailableAnimals() {
+  ulEl = document.createElement('ul');
+  ulEl.id = animalObjectArray[i].type;
+  liEl = document.createElement('li');
+  var imgEl = document.createElement('img');
+  imgEl.src = '../assets/imganimals/' + animalObjectArray[i].type + '.jpg';
+  liEl.appendChild(imgEl);
+  ulEl.appendChild(liEl);
+  //create li elements with each object property
+  createLi('Name:', 'name');
+  createLi('Animal Type:', 'type');
+  createLi('Age:', 'age');
+  createLi('Color:', 'color');
+  divEl.appendChild(ulEl);
+}
+
+//function to get filter from select list, put value into filter var
 function filter(event) {
   event.preventDefault();
   //get selected option value
@@ -36,11 +65,17 @@ function filter(event) {
   var selectedOption = select.options[select.selectedIndex].value;
   console.log(selectedOption, ' here is the selected option value');
   typeFilter = selectedOption;
+  var divEl = document.getElementById('elements');
+  i = 0;
+  while (divEl.firstElementChild) {
+    console.log('removing ', divEl.firstElementChild.innerHTML);
+    divEl.removeChild(divEl.firstElementChild);
+  }
   render();
 }
 
 //helper function to create li - jen
-function createLi(i, text, property) {
+function createLi(text, property) {
   liEl = document.createElement('li');
   liEl.innerText = `${text} ` + animalObjectArray[i][property];
   ulEl.appendChild(liEl);
